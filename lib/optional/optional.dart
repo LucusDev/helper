571540ba9f9e42871c369ffd6d5234e8b0cc8080
Htetaungshine11 <newbtcearner@gmail.com>
2022-02-17 15:04:59 +0630

    auto method added

--- lib/optional/optional.dart
+++ lib/optional/optional.dart
@@ -5,4 +5,11 @@ part 'optional.freezed.dart';
 class Optional<E> with _$Optional<E> {
   const factory Optional.avaliable(E value) = Avaliable<E>;
   const factory Optional.notAvaliable() = NotAvaliable;
+
+  factory Optional.auto(E? value) {
+    if (value == null) {
+      return const Optional.notAvaliable();
+    }
+    return Optional.avaliable(value);
+  }
 }
