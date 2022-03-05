a35970e4315d0fac95fd88ce766ff64bbdb44f7e
Htetaungshine11 <newbtcearner@gmail.com>
2022-03-05 15:37:42 +0630

    added bloc State

--- lib/helper.dart
+++ lib/helper.dart
@@ -2,3 +2,4 @@ library helper;
 
 export 'package:helper/result/result.dart' show Result;
 export 'package:helper/optional/optional.dart' show Optional;
+export 'package:helper/bloc_state/bloc_state.dart' show BlocState;
